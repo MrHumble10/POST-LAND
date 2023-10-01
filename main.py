79afab74712954a73f17ef9b7ad2f4e550a617b8
MrHumble10 <139639689+MrHumble10@users.noreply.github.com>
2023-10-01 13:03:37 -0300

    commit main file

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI_URL", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
