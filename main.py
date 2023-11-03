75cacdaf6a71759c5a77dae360b24c0e90e39760
MrHumble10 <139639689+MrHumble10@users.noreply.github.com>
2023-11-03 18:02:53 -0300

    modify requirements

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from functools import wraps
 from flask_gravatar import Gravatar
 import os
 from notifications import send_email
-from sounds import SoundWaves
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -351,41 +350,7 @@ def to_morse(s):
 def from_morse(s):
     return ''.join(CODE_REVERSED.get(i) for i in s.split())
 
-# ___________________________morse sound_____________________________________>
-new_sound = SoundWaves()
 
-
-convert_dict = {
-    "A": [1, 2], "B": [2, 1, 1, 1], "C": [2, 1, 2, 1], "D": [2, 1, 1],
-    "E": [1], "F": [1, 1, 2, 1], "G": [2, 2, 1], "H": [1, 1, 1, 1],
-    "I": [1, 1], "J": [1, 2, 2, 2], "K": [2, 1, 2], "L": [1, 2, 1, 1],
-    "M": [2, 2], "N": [2, 1], "O": [2, 2, 2], "P": [1, 2, 2, 1],
-    "Q": [2, 2, 1, 2], "R": [1, 2, 1], "S": [1, 1, 1], "T": [2],
-    "U": [1, 1, 2], "V": [1, 1, 1, 2], "W": [1, 2, 2], "X": [2, 1, 1, 2],
-    "Y": [2, 1, 2, 2], "Z": [2, 2, 1, 1],
-    "0": [2, 2, 2, 2, 2], "1": [1, 2, 2, 2, 2], "2": [1, 1, 2, 2, 2],
-    "3": [1, 1, 1, 2, 2], "4": [1, 1, 1, 1, 2], "5": [1, 1, 1, 1, 1],
-    "6": [2, 1, 1, 1, 1], "7": [2, 2, 1, 1, 1], "8": [2, 2, 2, 1, 1],
-    "9": [2, 2, 2, 2, 1]
-}
-
-
-def return_sound(sound_dict: dict, convert: str) -> None:
-    """returns the sound of the written letters and numbers.
-    :param sound_dict: the dictionary containing letters and numbers
-    :param convert: the actual string typed by the user to convert to morse code.
-    :return None
-    """
-
-    convert = list(convert)
-
-    for char in convert:
-        char = char.capitalize()
-        if char in sound_dict:
-            value = sound_dict[char]
-            new_sound.save_char(value)
-        else:
-            new_sound.add_longer_pause()
 @app.route('/morse', methods=['GET', "POST"])
 def morse():
     logged_in = current_user.is_authenticated
@@ -395,11 +360,8 @@ def morse():
     if request.method == 'POST':
         if request.form['p'] == 'from_morse':
             p = from_morse(request.form['message'])
-        elif request.form['p'] == 'to_morse':
+        else:
             p = to_morse(request.form['message'])
-        elif request.form['p'] == 'morse_sound':
-            p = return_sound(convert_dict, request.form['message'])
-            new_sound.save_wav_format()
         return render_template("morse.html", f=p, logged_in=current_user.is_authenticated)
     return render_template("morse.html", logged_in=current_user.is_authenticated)
 
